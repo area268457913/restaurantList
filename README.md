db3234dfa84d5482b94cd9d4f381d8a2b164af71
area268457913 <area268457913@hotmail.com>
2021-05-07 01:36:32 +0800

    feat: restaurant_list

---
+++ README.md
@@ -0,0 +1,37 @@
+# restaurant List
+
+利用 Node.js 和 Express 還有 Bootstrap所 建立一個簡單的網路應用程式
+
+## 功能
+
+- 使用者可以搜尋想找的餐廳
+- 使用者可以先觀看店名、照片、評價、餐廳類型
+- 使用者點選餐廳能看餐廳詳細資料
+
+## 環境建置與需求
+
+- Express 4.17.1
+- Express-Handlebars 5.3.0
+- Node.js v10.15.0
+
+## 專案安裝流程
+
+下載檔案
+
+git clone https://github.com/area268457913/restaurant.git
+
+進入專案目錄
+
+cd restaurant
+
+安裝套件
+
+npm install
+
+執行
+
+npm run dev
+
+##  專案開發人員
+
+area268457913
---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+app.engine('handlebars', exphbs({ defaultlayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === id)
+  res.render('show', { restaurant: restaurant })
+})
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+app.listen(port, () => {
+  console.log(`Express is listening on http:/localhost:${port}`)
+})
