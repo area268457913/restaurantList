7fcf54f3d54ee157df5f11167e9a621cbc3a7940
area268457913 <area268457913@hotmail.com>
2021-05-09 23:35:44 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
+    .sort({ _id: 'asc' }) //根據 _id 升冪排序
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
 
