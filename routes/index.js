c2237620bb7d6c0a7f2dba3494089d513c3f0227
area268457913 <area268457913@hotmail.com>
2021-06-01 00:16:51 +0800

    feat: add user modules & login page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 router.use('/restaurants', restaurants)
 router.use('/', home)
+router.use('/users', users)
 
 module.exports = router
